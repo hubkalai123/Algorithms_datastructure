7f792cb035a1598c3699e248a002976e89f20afc
gitkalaiarasu <codewithkalaiarasu.com>
2024-06-09 18:00:03 +0530

    update all code

--- Frequencypatters.js
+++ Frequencypatters.js
@@ -6,11 +6,14 @@ function FrequecyPatters(arr1, arr2) {
   let FrequecyPatters2 = {};
 
   for (let val of arr1) {
+    //if it occur obj in one 
     FrequecyPatters1[val] = (FrequecyPatters1[val] || 0) + 1;
   }
   for (let val of arr2) {
     FrequecyPatters2[val] = (FrequecyPatters2[val] || 0) + 1;
   }
+  console.log(FrequecyPatters1);
+  console.log(FrequecyPatters2);
   for (let key in FrequecyPatters1) {
     if (!(key ** 2 in FrequecyPatters2)) {
       return "false";
